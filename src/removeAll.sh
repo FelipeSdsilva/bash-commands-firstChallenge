0f1a0c8089b643c1e24582ca99ba27a04b0e835e
Felipe Sousa <felipe.fps09@hotmail.com>
2023-03-17 17:39:07 -0300

    Add file who Remove all users, groups and folders

---
+++ src/removeAll.sh
@@ -0,0 +1,18 @@
+userdel -rf carlos
+userdel -rf maria
+userdel -rf joao
+userdel -rf debora
+userdel -rf sebastiana
+userdel -rf roberto
+userdel -rf josefina
+userdel -rf amanda
+userdel -rf rogerio
+
+rm -rf publico/
+rm -rf adm/
+rm -rf ven/
+rm -rf sec/
+
+groupdel GRP_ADM
+groupdel GRP_VEN
+groupdel GRP_SEC
